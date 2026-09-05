c53c878689bfa964a9e4d458327bbac857fe06db
310tmsy-hub <310tmsy@gmail.com>
2026-09-06 02:49:58 +0900

    Create config.js

---
+++ api/config.js
@@ -0,0 +1,16 @@
+module.exports = async function handler(req, res) {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json; charset=utf-8');
+  res.setHeader('Cache-Control', 'no-store');
+
+  res.end(JSON.stringify({
+    supabaseUrl:
+      process.env.SUPABASE_PROJECT_URL ||
+      process.env.SUPABASE_URL ||
+      '',
+
+    supabaseAnonKey:
+      process.env.SUPABASE_ANON_KEY ||
+      ''
+  }));
+};
